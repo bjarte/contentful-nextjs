84b694c5cdf31be1bddc71568e5d940c3e532e34
Bjarte Aune Olsen <bjarte@auneolsen.no>
2023-06-10 12:16:16 +0200

    Fixed a typo in live preview code

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/index.css'
 import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
 
-function MyApp({ Component, pageProps }) {
-  <ContentfulLivePreviewProvider locale="en-US">
-    return <Component {...pageProps} />
-  </ContentfulLivePreviewProvider>
+function App({ Component, pageProps }) {
+  return (
+    <ContentfulLivePreviewProvider locale="en-US">
+      <Component {...pageProps} />
+    </ContentfulLivePreviewProvider>
+  )
 }
 
-export default MyApp
+export default App
